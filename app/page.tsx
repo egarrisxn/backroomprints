05f6537bea2eeb6cb769884efcee8fa072ac26b5
egarrisxn <egarrisxn@gmail.com>
2026-02-04 05:21:57 -0500

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 import Link from "next/link";
 export default function Home() {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center gap-4 text-2xl font-bold">
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-4 text-xl">
       <Link href="/one" className="underline">
-        Page One
+        One
+      </Link>
+      <Link href="/two" className="underline">
+        Two
       </Link>
       <Link href="/three" className="underline">
-        Page One
-      </Link>
-      <Link href="/three" className="underline">
-        Page Three
+        Three
       </Link>
     </div>
   );
